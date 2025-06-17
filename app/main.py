d6a6763691bd36db9b11c51ce32595ac0322a639
onjambola <onja.mbola.andrianirimanjaka@gmail.com>
2025-06-17 12:30:30 +0300

    First commit

---
+++ app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+templates = Jinja2Templates(directory="app/templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+def login_page(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+
+@app.post("/login", response_class=HTMLResponse)
+def handle_login(request: Request, email: str = Form(...), password: str = Form(...)):
+    if email == "avocat@legalhub.fr" and password == "1234":
+        user = {"nom": "Dupont"}
+        return templates.TemplateResponse("dashboard_avocat.html", {"request": request, "user": user})
+    else:
+        return templates.TemplateResponse("login.html", {"request": request, "error": "Identifiants incorrects"})
